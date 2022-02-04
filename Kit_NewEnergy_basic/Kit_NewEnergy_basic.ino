33d6405d8c5d86f29922c42fd1f6c67ca01f5dca
jeong-inn <jung1nn@naver.com>
2022-02-04 11:13:41 +0900

    fix : windturbine oled display

--- Kit_NewEnergy_basic/Kit_NewEnergy_basic.ino
+++ Kit_NewEnergy_basic/Kit_NewEnergy_basic.ino
@@ -21,30 +21,30 @@ void setup_oled(){
 
 void display_oled(){
   int solar_voltage_value = analogRead(A3);
-  Serial.print("태양관 센서 : ");
+  Serial.print("태양광 센서 : ");
   Serial.println(solar_voltage_value);
 
-    char text1[32] = "S : ";
-    char value1[32];
-    String str = String(solar_voltage_value * c_value, DEC);
-    str.toCharArray(value1, 6);
-    strcat(text1, value1);
-    strcat(text1, "V");
+  char text1[32] = "S : ";
+  char value1[32];
+  String str = String(solar_voltage_value * c_value, DEC);
+  str.toCharArray(value1, 6);
+  strcat(text1, value1);
+  strcat(text1, "V");
+  
+  int windturbine_voltage_value = analogRead(A5);
+  Serial.print("풍력 센서 : ");
+  Serial.println(windturbine_voltage_value);
+  Serial.println("-------------------");
 
-    int windturbine_voltage_value = analogRead(A5);
-    Serial.print("풍력 센서 : ");
-    Serial.print(windturbine_voltage_value);
-    Serial.println("-------------------");
+  char text2[32] = "W : ";
+  char value2[32];
+  String str2 = String(windturbine_voltage_value * c_value, DEC);
+  str2.toCharArray(value2, 6);
+  strcat(text2, value2);
+  strcat(text2, "V");
 
-    char text2[32] = "W : ";
-    char value2[32];
-    String str2 = String(windturbine_voltage_value * c_value, DEC);
-    str.toCharArray(value2, 6);
-    strcat(text2, value2);
-    strcat(text2, "V");
-
-    oled.setLine(1, "* ECO Energy *");
-    oled.setLine(2, text1);
-    oled.setLine(3, text2);
-    oled.display();
+  oled.setLine(1, "* ECO Energy *");
+  oled.setLine(2, text1);
+  oled.setLine(3, text2);
+  oled.display();
 }
